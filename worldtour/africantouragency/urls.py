3ec8288917aa9ecb71d751e32f32bf78967e8f94
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2025-10-08 22:10:00 +0100

    forms in django

--- worldtour/africantouragency/urls.py
+++ worldtour/africantouragency/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 # Define a list of url patterns
 urlpatterns = [
-    path('', views.index),
-    path('styling', views.styling)
+    path('', views.index, name='home'),
+    # path('home', views.inde),
+    path('styling/', views.styling),
+    path('form-home/', views.home_view, name='form-app'),
+    path('contact/', views.contact_view, name='contact'),
+    path('contact-success/', views.contact_success_view, name='contact-success'),
 ]
--- worldtour/africantouragency/views.py
+++ worldtour/africantouragency/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tour
+from .forms import ContactForm
 
 
 # Create your views here.
@@ -13,3 +14,26 @@ def index(request):
 
 def styling(request):
     return render(request, 'django_app/index.html')
+
+
+# Home page for the form app
+def home_view(request):
+    return render(request, 'form_app/home.html')
+
+
+# handle the contact form
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.send_email()
+            return redirect('contact-success')
+    else:
+        form = ContactForm()
+    context = {'form': form}
+    return render(request, 'form_app/contact.html', context)
+
+
+# define the contact success page
+def contact_success_view(request):
+    return render(request, 'form_app/success.html')
